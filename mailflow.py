c6c2a1025495fda98638ddead538aff53ebf5599
Evandro Couto <vandocouto@gmail.com>
2015-09-10 09:24:19 -0300

    Quarta versão

--- mailflow.py
+++ mailflow.py
@@ -10,33 +10,32 @@ def mailflow():
     
     # executando o mailflow
     try:
-        # até 2 minutos
-        envia=enviamail("mailflow@%s" %sys.argv[1])
-        time.sleep(110)
+	envia=enviamail("mailflow@%s" %sys.argv[1])
+        time.sleep(10)
         valida=checkmail()
-        # acima de 2 minutos
-        if (str(valida) == "Erro-3"):
-            time.sleep(230)
-            valida=checkmail()
-	    if (str(valida) > '0:02:00') and (str(valida) < '0:04:00'):
-            	arq = open('%s.log' %sys.argv[1] , 'w')
-            	arq.write("%s - ATENCAO" %valida)
-            	return valida
-	    elif (str(valida) > '0:04:00'):
-            	arq = open('%s.log' %sys.argv[1] , 'w')
-            	arq.write("%s - MEDIA" %valida)
-            	return valida
+
+	while (str(valida) == "Erro-3"):
+           time.sleep(5)
+	   valida=checkmail()
+
+	# acima de 2 minutos
+	if (str(valida) < '0:01:30'):
+       	   arq = open('%s.log' %sys.argv[1] , 'w')
+           arq.write("%s - OK" %valida)
+           return valida
+	elif (str(valida) < '0:04:00'):
+           arq = open('%s.log' %sys.argv[1] , 'w')
+           arq.write("%s - MEDIA" %valida)
+           return valida
         else:
 	    arq = open('%s.log' %sys.argv[1] , 'w')
-            arq.write("%s - OK" %valida)
+            arq.write("%s - ALTA" %valida)
             return valida
     except:
 	arq = open('%s.log' %sys.argv[1] , 'w')
         arq.write("%s - DESASTRE" %valida)
         return valida
     
-        
-
 print mailflow()
 
 
